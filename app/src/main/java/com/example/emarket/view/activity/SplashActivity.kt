88903a07e54b8d7e8ff8e61db1855c92a784ace3
yao <yuanyao721@gmail.com>
2023-08-10 17:34:05 -0500

    create logo and splash screen

---
+++ app/src/main/java/com/example/emarket/view/activity/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.emarket.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.emarket.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}

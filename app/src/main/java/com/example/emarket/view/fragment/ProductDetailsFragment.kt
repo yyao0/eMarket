82d88d15c0de7207871fd25474a9070108c0307f
yao <yuanyao721@gmail.com>
2023-08-20 23:55:51 -0500

    debug cart counter widget

--- app/src/main/java/com/example/emarket/view/fragment/ProductDetailsFragment.kt
+++ app/src/main/java/com/example/emarket/view/fragment/ProductDetailsFragment.kt
@@ -1,6 +1,7 @@
 package com.example.emarket.view.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,12 +12,13 @@ import com.example.emarket.model.local.dao.CartDao
 import com.example.emarket.model.local.entity.Product
 import com.example.emarket.presenter.ProductDetailsContract
 import com.example.emarket.presenter.ProductDetailsPresenter
+import com.example.emarket.view.CartWidgetView
 import com.example.emarket.view.adapter.ImagePagerAdapter
 import com.example.emarket.view.adapter.ReviewAdapter
 import com.example.emarket.view.adapter.SpecificationAdapter
 import com.google.android.material.tabs.TabLayout
 
-class ProductDetailsFragment : Fragment(), ProductDetailsContract.View {
+class ProductDetailsFragment : Fragment(), ProductDetailsContract.View, CartWidgetView.OnQuantityChangeListener {
     private lateinit var binding: FragmentProductDetailsBinding
     private lateinit var presenter: ProductDetailsPresenter
     private lateinit var imageAdapter: ImagePagerAdapter
@@ -24,6 +26,7 @@ class ProductDetailsFragment : Fragment(), ProductDetailsContract.View {
     private lateinit var specificationAdapter: SpecificationAdapter
     private lateinit var product: Product
     private lateinit var cartDao: CartDao
+    private lateinit var cartWidgetView: CartWidgetView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,6 +46,8 @@ class ProductDetailsFragment : Fragment(), ProductDetailsContract.View {
         presenter = ProductDetailsPresenter(this, requireContext())
         cartDao = CartDao(requireContext())
         presenter.getProductDetailsRemote(product.product_id)
+        cartWidgetView  = binding.cartCounter
+        cartWidgetView.setOnQuantityChangeListener(this)
         addProductToCart()
     }
 
@@ -85,12 +90,26 @@ class ProductDetailsFragment : Fragment(), ProductDetailsContract.View {
         binding.cartCounter.setProduct(product.product_id)
         binding.cartCounter.updateQuantityText()
         val quantity = presenter.getProductQuantity(product.product_id)
-        if (quantity == 0){
-        binding.tvAddCart.setOnClickListener {
-            cartDao.insertOrUpdateCartItem(product, 1)
+        if (quantity == 0) {
+            binding.tvAddCart.visibility = View.VISIBLE
+            binding.cartCounter.visibility = View.GONE
+        } else {
             binding.tvAddCart.visibility = View.GONE
             binding.cartCounter.visibility = View.VISIBLE
         }
+        binding.tvAddCart.setOnClickListener {
+            cartDao.insertOrUpdateCartItem(product, 1)
+            binding.cartCounter.updateQuantityText()
+            binding.tvAddCart.visibility = View.GONE
+            binding.cartCounter.visibility = View.VISIBLE
+        }
+    }
+
+    override fun onQuantityChanged(newQuantity: Int) {
+        Log.i("tag", newQuantity.toString())
+        if (newQuantity == 0) {
+            binding.tvAddCart.visibility = View.VISIBLE
+            binding.cartCounter.visibility = View.GONE
         } else {
             binding.tvAddCart.visibility = View.GONE
             binding.cartCounter.visibility = View.VISIBLE
